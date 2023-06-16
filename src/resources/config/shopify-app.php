b3736be91d32a4ce427c8360e8f050d90cbbac88
angel-rec-ct <103230482+angel-rec-ct@users.noreply.github.com>
2023-06-16 00:20:12 -0600

    Adding custom url to config.

--- src/resources/config/shopify-app.php
+++ src/resources/config/shopify-app.php
@@ -3,6 +3,18 @@
 return [
     /*
     |--------------------------------------------------------------------------
+    | Custom URL
+    |--------------------------------------------------------------------------
+    |
+    | Quick fix for our use case.
+    |
+    */
+
+    'custom_url' => (string) env('SHOPIFY_CUSTOM_URL', ''),
+    
+    
+    /*
+    |--------------------------------------------------------------------------
     | Debug Mode
     |--------------------------------------------------------------------------
     |
@@ -56,13 +68,13 @@ return [
     */
 
     'route_names' => [
-        'home' => env('SHOPIFY_ROUTE_NAME_HOME', 'home'),
-        'authenticate' => env('SHOPIFY_ROUTE_NAME_AUTHENTICATE', 'authenticate'),
-        'authenticate.token' => env('SHOPIFY_ROUTE_NAME_AUTHENTICATE_TOKEN', 'authenticate.token'),
-        'billing' => env('SHOPIFY_ROUTE_NAME_BILLING', 'billing'),
-        'billing.process' => env('SHOPIFY_ROUTE_NAME_BILLING_PROCESS', 'billing.process'),
+        'home'                 => env('SHOPIFY_ROUTE_NAME_HOME', 'home'),
+        'authenticate'         => env('SHOPIFY_ROUTE_NAME_AUTHENTICATE', 'authenticate'),
+        'authenticate.token'   => env('SHOPIFY_ROUTE_NAME_AUTHENTICATE_TOKEN', 'authenticate.token'),
+        'billing'              => env('SHOPIFY_ROUTE_NAME_BILLING', 'billing'),
+        'billing.process'      => env('SHOPIFY_ROUTE_NAME_BILLING_PROCESS', 'billing.process'),
         'billing.usage_charge' => env('SHOPIFY_ROUTE_NAME_BILLING_USAGE_CHARGE', 'billing.usage_charge'),
-        'webhook' => env('SHOPIFY_ROUTE_NAME_WEBHOOK', 'webhook'),
+        'webhook'              => env('SHOPIFY_ROUTE_NAME_WEBHOOK', 'webhook'),
     ],
 
     /*
@@ -141,10 +153,6 @@ return [
     // Leaving empty will use the latest version - not recommended in production.
     'appbridge_version' => env('SHOPIFY_APPBRIDGE_VERSION', 'latest'),
 
-    // Set a new CDN URL if you want to host the AppBridge JS yourself or unpkg goes down.
-    // DO NOT include a trailing slash.
-    'appbridge_cdn_url' => env('SHOPIFY_APPBRIDGE_CDN_URL', 'https://unpkg.com'),
-
     /*
     |--------------------------------------------------------------------------
     | Shopify App Name
@@ -201,7 +209,7 @@ return [
     |
     */
 
-    'api_scopes' => env('SHOPIFY_API_SCOPES', 'read_products,write_products,read_themes'),
+    'api_scopes' => env('SHOPIFY_API_SCOPES', 'read_products,write_products'),
 
     /*
     |--------------------------------------------------------------------------
@@ -237,7 +245,7 @@ return [
     |
     */
 
-    'api_time_store' => env('SHOPIFY_API_TIME_STORE', \Gnikyt\BasicShopifyAPI\Store\Memory::class),
+    'api_time_store' => env('SHOPIFY_API_TIME_STORE', \Osiset\BasicShopifyAPI\Store\Memory::class),
 
     /*
     |--------------------------------------------------------------------------
@@ -249,7 +257,7 @@ return [
     |
     */
 
-    'api_limit_store' => env('SHOPIFY_API_LIMIT_STORE', \Gnikyt\BasicShopifyAPI\Store\Memory::class),
+    'api_limit_store' => env('SHOPIFY_API_LIMIT_STORE', \Osiset\BasicShopifyAPI\Store\Memory::class),
 
     /*
     |--------------------------------------------------------------------------
@@ -261,7 +269,7 @@ return [
     |
     */
 
-    'api_deferrer' => env('SHOPIFY_API_DEFERRER', \Gnikyt\BasicShopifyAPI\Deferrers\Sleep::class),
+    'api_deferrer' => env('SHOPIFY_API_DEFERRER', \Osiset\BasicShopifyAPI\Deferrers\Sleep::class),
 
     /*
     |--------------------------------------------------------------------------
@@ -270,9 +278,9 @@ return [
     |
     | This option is for initializing the BasicShopifyAPI package yourself.
     | The first param injected in is the current options.
-    |    (\Gnikyt\BasicShopifyAPI\Options)
+    |    (\Osiset\BasicShopifyAPI\Options)
     | The second param injected in is the session (if available) .
-    |    (\Gnikyt\BasicShopifyAPI\Session)
+    |    (\Osiset\BasicShopifyAPI\Session)
     | The third param injected in is the current request input/query array.
         (\Illuminate\Http\Request::all())
     | With all this, you can customize the options, change params, and more.
@@ -414,8 +422,8 @@ return [
     */
 
     'job_queues' => [
-        'webhooks' => env('WEBHOOKS_JOB_QUEUE', null),
-        'scripttags' => env('SCRIPTTAGS_JOB_QUEUE', null),
+        'webhooks'           => env('WEBHOOKS_JOB_QUEUE', null),
+        'scripttags'         => env('SCRIPTTAGS_JOB_QUEUE', null),
         'after_authenticate' => env('AFTER_AUTHENTICATE_JOB_QUEUE', null),
     ],
 
@@ -486,48 +494,6 @@ return [
          */
         'shops' => 'users',
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Checking theme compatibility
-    |--------------------------------------------------------------------------
-    |
-    | It is necessary to check if your application is compatible with
-    | the theme app blocks.
-    |
-    */
-
-    'theme_support' => [
-        /**
-         * Specify the name of the template the app will integrate with
-         */
-        'templates' => ['product', 'collection', 'index'],
-        /**
-         * Interval for caching the request: minutes, seconds, hours, days, etc.
-         */
-        'cache_interval' => 'hours',
-        /**
-         * Cache duration
-         */
-        'cache_duration' => '12',
-         /**
-         * At which levels of theme support the use of "theme app extension" is not available
-         * and script tags will be installed.
-         * Available levels: FULL, PARTIAL, UNSUPPORTED.
-         */
-        'unacceptable_levels' => [
-            Osiset\ShopifyApp\Objects\Enums\ThemeSupportLevel::UNSUPPORTED
-        ]
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Session token refresh
-    |--------------------------------------------------------------------------
-    |
-    | For AppBridge, how often to refresh the session token for the user.
-    |
-    */
 
     'session_token_refresh_interval' => env('SESSION_TOKEN_REFRESH_INTERVAL', 2000),
 
